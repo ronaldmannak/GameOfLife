e235c48beeac6dd7b7ca0509b2452d030c0c5acc
Ronald Mannak <ronaldmannak@me.com>
2014-07-14 19:33:00 -0700

    Added basic graphics

--- GameOfLife/GameScene.swift
+++ GameOfLife/GameScene.swift
@@ -9,36 +9,71 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+
+    let _gridWidth = 400
+    let _gridHeight = 300
+    let _numRows = 8
+    let _numCols = 10
+    let _gridLowerLeftCorner: CGPoint = CGPoint(x: 158, y: 10)
+    
+    let _populationLabel = SKLabelNode(text: "Population")
+    let _generationLabel = SKLabelNode(text: "Generation")
+    var _populationValueLabel = SKLabelNode(text: "0")
+    var _generationValueLabel = SKLabelNode(text: "0")
+    var _playButton = SKSpriteNode(imageNamed: "play.png")
+    var _pauseButton = SKSpriteNode(imageNamed: "pause.png")
+   
+    override func didMoveToView(view: SKView!) {
+        let background = SKSpriteNode(imageNamed: "background.png")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.size = self.size
+        background.zPosition = -2
+        self.addChild(background)
         
-        self.addChild(myLabel)
+        let gridBackground = SKSpriteNode(imageNamed: "grid.png")
+        gridBackground.size = CGSize(width: _gridWidth, height: _gridHeight)
+        gridBackground.zPosition = -1
+        gridBackground.anchorPoint = CGPoint(x: 0, y: 0)
+        gridBackground.position = _gridLowerLeftCorner
+        self.addChild(gridBackground)
+        
+        _playButton.position = CGPoint(x: 79, y: 290)
+        _playButton.setScale(0.5)
+        self.addChild(_playButton)
+        _pauseButton.position = CGPoint(x: 79, y: 250)
+        _pauseButton.setScale(0.5)
+        self.addChild(_pauseButton)
+        let balloon = SKSpriteNode(imageNamed: "balloon.png")
+        balloon.position = CGPoint(x: 79, y: 170)
+        balloon.setScale(0.5)
+        self.addChild(balloon)
+        let microscope = SKSpriteNode(imageNamed: "microscope.png")
+        microscope.position = CGPoint(x: 79, y: 70)
+        microscope.setScale(0.4)
+        self.addChild(microscope)
+        _populationLabel.position = CGPoint(x: 79, y: 190)
+        _populationLabel.fontName = "Courier"
+        _populationLabel.fontSize = 12
+        _populationLabel.fontColor = UIColor(red: 0, green: 0.2, blue: 0, alpha: 1)
+        self.addChild(_populationLabel)
+        _generationLabel.position = CGPoint(x: 79, y: 160)
+        _generationLabel.fontName = "Courier"
+        _generationLabel.fontSize = 12
+        _generationLabel.fontColor = UIColor(red: 0, green: 0.2, blue: 0, alpha: 1)
+        self.addChild(_generationLabel)
+        _populationValueLabel.position = CGPoint(x: 79, y: 175)
+        _populationValueLabel.fontName = "Courier"
+        _populationValueLabel.fontSize = 12
+        _populationValueLabel.fontColor = UIColor(red: 0, green: 0.2, blue: 0, alpha: 1)
+        self.addChild(_populationValueLabel)
+        _generationValueLabel.position = CGPoint(x: 79, y: 145)
+        _generationValueLabel.fontName = "Courier"
+        _generationValueLabel.fontSize = 12
+        _generationValueLabel.fontColor = UIColor(red: 0, green: 0.2, blue: 0, alpha: 1)
+        self.addChild(_generationValueLabel)
+        
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
